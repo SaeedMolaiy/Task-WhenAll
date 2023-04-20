202ce920f1128842a1762590fa2078a9c6124db4
Saeed Molaiy <101869414+SaeedMolaiy@users.noreply.github.com>
2023-04-20 12:38:40 -0700

    Task WhenAll

---
+++ TaskWhenAll/Program.cs
@@ -0,0 +1,31 @@
+﻿
+await DoAll();
+
+async Task DoIt()
+{
+    await Task.Delay(1000);
+    Console.WriteLine("DoIt");
+}
+
+async Task DoItAgain()
+{
+    await Task.Delay(1500);
+    Console.WriteLine("IDoItAgain");
+}
+
+async Task DoSomething()
+{
+    await Task.Delay(300);
+    Console.WriteLine("IDoSomething");
+}
+
+void WhenAll()
+{
+    Console.WriteLine("All Done..");
+}
+
+async Task DoAll()
+{
+    await Task.WhenAll(DoIt(), DoItAgain(), DoSomething());
+    WhenAll();
+}
